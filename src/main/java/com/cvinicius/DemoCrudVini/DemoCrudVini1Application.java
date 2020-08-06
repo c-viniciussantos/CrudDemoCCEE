16b993defe42400c3eaeb3c7780257512ccf2607
Vinicius Santos <c.viniciussantos@outlook.com>
2020-08-06 11:46:27 -0300

    Projeto inicial

---
+++ src/main/java/com/cvinicius/DemoCrudVini/DemoCrudVini1Application.java
@@ -0,0 +1,15 @@
+package com.cvinicius.DemoCrudVini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoCrudVini1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudVini1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvinicius/DemoCrudVini/DemoCrudVini1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvinicius.DemoCrudVini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudVini1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
